861897c2199d86e50ca97019d4ad657c9c307749
tajud99n <giwa208@gmail.com>
2018-09-04 17:54:11 +0100

    DB Config added

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_802eefef7fc0a8c'),
+            'username' => env('DB_USERNAME', 'b24d6236ae446e'),
+            'password' => env('DB_PASSWORD', 'd6cdb583'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
